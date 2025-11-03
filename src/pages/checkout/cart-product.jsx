caca8796d31e684aef4b38cb80c08ed413f039f5
Aneeq Ahmed <161975416+CodeAneeq@users.noreply.github.com>
2025-11-03 05:08:32 +0500

    Update cart-product.jsx

--- src/pages/checkout/cart-product.jsx
+++ src/pages/checkout/cart-product.jsx
@@ -6,29 +6,6 @@ import ProductQuantityCounter from "../../components/cart/product-quantity-count
 import useShoppingCart from "../../hooks/use-shopping-cart";
 import { Helpers } from "../../services/helper";
 
-// export const CartProduct = ({data}) => {
-
-//   const { addToCart, decreaseProductQuantityInCart, removeFromCart, getCartCount, getCartProducts, getCartProductQuantity, getCartSingleProducts, clearCart, } = useShoppingCart();
-//   const totalPrice = data.product.price * data.quantity;
-//   return (
-//     <tr className={`${styles.table_row}`}>
-//       <td>
-//         <div className={`${styles.product_cell}`}>
-//           <div className={`${styles.img_container}`}>
-//             <img className={`${styles.product_img}`} src={data.product?.images?.[0]} alt="" />
-//             <img className={`${styles.cross_icon}`} src={iconCancel} alt=""  onClick={() => removeFromCart(data?.product)}/>
-//           </div>
-//           <p className="m-0 ps-2">{data.product.name}</p>
-//         </div>
-//       </td>
-//       <td>{Helpers.priceFormatter(data?.product?.price)}</td>
-//       <td>
-//         <ProductQuantityCounter small qty={data.quantity} onIncrement={() => addToCart(data?.product)} onDecrement={() => decreaseProductQuantityInCart(data?.product)}></ProductQuantityCounter>
-//       </td>
-//       <td>{Helpers.priceFormatter(totalPrice)}</td>
-//     </tr>
-//   );
-// };
 export const CartProduct = ({data}) => {
   const { addToCart, decreaseProductQuantityInCart, removeFromCart } = useShoppingCart();
   const totalPrice = data.product.price * data.quantity;
@@ -66,3 +43,5 @@ export const CartProduct = ({data}) => {
     </tr>
   );
 };
+
+export default CartProduct
